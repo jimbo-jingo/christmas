6aa241c00798a6107ecbf992cca0308f591b29ba
jimbo-jingo <jmfinke@gmail.com>
2024-12-12 10:21:10 +0100

    Create jan.py

---
+++ jan.py
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Santa's Greeting</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="santa">
+            <img src="https://example.com/santa-image.jpg" alt="Santa Claus" id="santaImage">
+        </div>
+        <h1 id="greeting">Ho Ho Ho! Merry Christmas!</h1>
+        <button id="changeGreetingBtn">Click for a New Greeting</button>
+    </div>
+
+    <script src="script.js"></script>
+</body>
+</html>
